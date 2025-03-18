1de2a6613fd776773845baf4d9fcafcfbe5d0e06
jenri019 <j.enrique209436@hotmail.com>
2025-03-18 16:44:31 -0600

    feat: Añadiendo modelo pra la tabl de generos

--- back/generos/models.py
+++ back/generos/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+from django.db.models.fields import CharField, AutoField
+    
+class Genero(models.Model):
+    GENERO_ID = AutoField(primary_key=True)
+    GENERO_NOMBRE = CharField(max_length=20)
