48210a3d5c033b01bd66c9c9dccfa19cf5742672
jenri019 <j.enrique209436@hotmail.com>
2025-03-19 13:27:11 -0600

    feat: Añadiendo modelo para los items

--- back/gestor/models.py
+++ back/gestor/models.py
@@ -1,7 +1,17 @@
 from django.db import models
-from django.db.models.fields import CharField, AutoField
+from django.db.models.fields import CharField, TextField, URLField, IntegerField, BooleanField, AutoField
 
 # Create your models here.
 class Genero(models.Model):
     id = AutoField(primary_key=True)
     name = CharField(max_length=20)
+
+class Item(models.Model):
+    id = AutoField(primary_key=True)
+    title = CharField(max_length=100)
+    tipo = CharField(max_length=6, default="")
+    description = TextField(max_length = 300, blank=True)
+    url = URLField()
+    current_chapter = IntegerField()
+    on_going = BooleanField(default=True)
+    generos = models.TextField(default="")
