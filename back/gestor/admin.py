f32ce1ad32dbb3dc0ea13a001ed1e099d6e15986
jenri019 <j.enrique209436@hotmail.com>
2025-03-19 20:32:55 -0600

    feat: modificandonombre del modelo en los imports

--- back/gestor/admin.py
+++ back/gestor/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Genero, Item, Types
+from .models import Genero, Item, Type
 
 admin.site.register(Genero)
 admin.site.register(Item)
-admin.site.register(Types)
+admin.site.register(Type)
