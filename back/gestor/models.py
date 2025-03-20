0762609dad5f1a4ea13cb9e2cf363e1ddfd79703
jenri019 <j.enrique209436@hotmail.com>
2025-03-19 20:24:31 -0600

    feat: modificando los modelos, cambiando nomvres y añadiendo los tipos

--- back/gestor/models.py
+++ back/gestor/models.py
@@ -3,19 +3,19 @@ from django.db.models.fields import CharField, TextField, URLField, IntegerField
 
 # Create your models here.
 class Genero(models.Model):
-    id = AutoField(primary_key=True)
-    name = CharField(max_length=20)
+    GENERO_ID = AutoField(primary_key=True)
+    GENERO_NAME = CharField(max_length=20)
+
+class Type(models.Model):
+    TYPE_ID = AutoField(primary_key=True)
+    TYPE_NAME = CharField(max_length=20)
 
 class Item(models.Model):
-    id = AutoField(primary_key=True)
-    title = CharField(max_length=100)
-    tipo = CharField(max_length=6, default="")
-    description = TextField(max_length = 300, blank=True)
-    url = URLField()
-    current_chapter = IntegerField()
-    on_going = BooleanField(default=True)
-    generos = models.TextField(default="")
-
-class Types(models.Model):
-    id = AutoField(primary_key=True)
-    name = CharField(max_length=20)
+    ITEM_ID = AutoField(primary_key=True)
+    ITEM_TITLE = CharField(max_length=100)
+    ITEM_TYPE = models.ForeignKey(Type, on_delete=models.CASCADE)
+    ITEM_ECRIPTION = TextField(max_length = 300, blank=True)
+    ITEM_URL = URLField()
+    ITEM_CURRENT_CHAPTER = IntegerField()
+    ITEM_ON_GOING = BooleanField(default=True)
+    ITEM_GENRES = models.TextField(default="")
