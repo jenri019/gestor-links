78d784b91067f856589bcc989dfc99660c92ca42
jenri019 <j.enrique209436@hotmail.com>
2025-03-21 13:13:17 -0600

    feat: añadiendo normalize y contenedores principales

--- front/src/app/pages/home-page/home-page.component.ts
+++ front/src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ManageItemComponent } from "../../components/manage-item/manage-item.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ManageItemComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
