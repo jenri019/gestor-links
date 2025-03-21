5a9fd2d3d9776fe8fecf4c15e73a19bf5619b6df
jenri019 <j.enrique209436@hotmail.com>
2025-03-21 12:34:29 -0600

    feat: adding first components to front

---
+++ front/src/app/components/manage-item/manage-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-item',
+  imports: [],
+  templateUrl: './manage-item.component.html',
+  styleUrl: './manage-item.component.scss'
+})
+export class ManageItemComponent {
+
+}
