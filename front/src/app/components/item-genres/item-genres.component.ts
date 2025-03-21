a7f7014393e51e01156c18f9d578a428d5025674
jenri019 <j.enrique209436@hotmail.com>
2025-03-21 13:43:01 -0600

    feat: añadiendo formulario de creacion edicion y ajustando diseño para responsivo

---
+++ front/src/app/components/item-genres/item-genres.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-genres',
+  imports: [],
+  templateUrl: './item-genres.component.html',
+  styleUrl: './item-genres.component.scss'
+})
+export class ItemGenresComponent {
+
+}
--- front/src/app/components/manage-item/manage-item.component.ts
+++ front/src/app/components/manage-item/manage-item.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ItemGenresComponent } from "../item-genres/item-genres.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-manage-item',
-  imports: [],
+  imports: [
+    CommonModule,
+    ItemGenresComponent
+  ],
   templateUrl: './manage-item.component.html',
   styleUrl: './manage-item.component.scss'
 })
 export class ManageItemComponent {
+  show_form: boolean = true;
 
+  handleChangeFormVisibility() {
+    this.show_form = !this.show_form;
+  }
 }
