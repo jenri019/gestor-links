2c8230b96e1c034b9e025f0743bf41b5f6603919
jenri019 <j.enrique209436@hotmail.com>
2025-03-24 11:26:37 -0600

    feat: dando estilos a las badge para los differentes generos

--- front/src/app/components/item-genres/item-genres.component.ts
+++ front/src/app/components/item-genres/item-genres.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { genres } from '../../data/genres';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-item-genres',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './item-genres.component.html',
   styleUrl: './item-genres.component.scss'
 })
 export class ItemGenresComponent {
-
+  genres: any[] = genres;
 }
