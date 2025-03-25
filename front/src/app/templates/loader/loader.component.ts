079fadc679c630855b61756f9f8f3409651300ea
jenri019 <j.enrique209436@hotmail.com>
2025-03-25 13:18:06 -0600

    feat: añadiendo estilos al loader

--- front/src/app/templates/loader/loader.component.ts
+++ front/src/app/templates/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './loader.component.scss'
 })
 export class LoaderComponent {
+  @Input() color: string = '#3f51b5'; // Color por defecto (puedes cambiarlo)
+  @Input() thickness: string = '3px'; // Grosor del spinner
+  @Input() speed: string = '1s'; // Velocidad de animación
 
+  @HostBinding('style.display') display = 'block';
+  @HostBinding('style.width') width = '100%';
+  @HostBinding('style.height') height = '100%';
 }
