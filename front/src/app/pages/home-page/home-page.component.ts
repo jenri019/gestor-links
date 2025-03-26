e5f96d059c0c53a72d0eaa2ef26a088ce392ab55
jenri019 <j.enrique209436@hotmail.com>
2025-03-26 14:02:31 -0600

    feat: obteniendo el listado de tipos

--- front/src/app/pages/home-page/home-page.component.ts
+++ front/src/app/pages/home-page/home-page.component.ts
@@ -2,12 +2,13 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ManageItemComponent } from "../../components/manage-item/manage-item.component";
 import { CardComponent } from "../../components/card/card.component";
 import { CommonModule } from '@angular/common';
-import { LoaderComponent } from "../../templates/loader/loader.component";
 import { GenresService } from '../../services/genres.service';
 import { Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { AppState, Genres } from '../../interfaces/state.interface';
+import { AppState, GenresTypes } from '../../interfaces/state.interface';
 import GenresActions from '../../store/actions/genres.actions';
+import { TypesService } from '../../services/types.service';
+import TypesActions from '../../store/actions/types.actions';
 
 @Component({
     selector: 'app-home-page',
@@ -22,7 +23,10 @@ import GenresActions from '../../store/actions/genres.actions';
 export class HomePageComponent implements OnInit, OnDestroy {
     isLoading: boolean = false;
     genres: any[] = [];
+    types: any[] = [];
+    
     private genresSubscription!: Subscription;
+    private typesSubscription!: Subscription;
     cards: any[] = [
         {
             id: 1,
@@ -43,19 +47,28 @@ export class HomePageComponent implements OnInit, OnDestroy {
 
     constructor(
         private _genresService: GenresService,
+        private _typesService: TypesService,
         private store: Store<AppState>
     ) { }
 
     ngOnInit(): void {
-        this.genresSubscription = this.store.select('genres').subscribe((genres: Genres) => {
+        this.genresSubscription = this.store.select('genres').subscribe((genres: GenresTypes) => {
             if (genres.flag) this.onGetGenres();
             else this.genres = genres.data;
+        });
+
+        this.typesSubscription = this.store.select('types').subscribe((types: GenresTypes) => {
+            if (types.flag) this.onGetTypes();
+            else this.types = types.data;
         });
     }
 
     ngOnDestroy(): void {
         if (this.genresSubscription) {
             this.genresSubscription.unsubscribe();
+        }
+        if (this.typesSubscription) {
+            this.typesSubscription.unsubscribe();
         }
     }
 
@@ -64,10 +77,19 @@ export class HomePageComponent implements OnInit, OnDestroy {
         this._genresService.onGetGenres().subscribe({
             next: (resp) => {
                 this.store.dispatch(GenresActions.set({ props: { data: resp.data, flag: false } }));
-                this.isLoading = false;
             },
             error: (err) => {
-                this.isLoading = false;
+            }
+        })
+    }
+
+    onGetTypes = () => {
+        this.isLoading = true;
+        this._typesService.onGetTypes().subscribe({
+            next: (resp) => {
+                this.store.dispatch(TypesActions.set({ props: { data: resp.data, flag: false } }));
+            },
+            error: (err) => {
             }
         })
     }
